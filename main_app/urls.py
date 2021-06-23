de140fa164ac19e5cbc48b889116b3f338d7180f
brandonhall96 <hall.bran96@gmail.com>
2021-06-23 12:24:12 -0400

    added cats folder with index.html, added cat data to views and created url

--- main_app/urls.py
+++ main_app/urls.py
@@ -5,7 +5,13 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
+    path('', views.index, name='index' ),
     path('about/', views.about, name='about' ), #views.about goes to views.py which returns render from about.html
-    path('index/', views.index, name='index' ) 
+    path('cats/', views.cats_index, name='cats_index' ) # Get cats folder. call cats_index from views.py
     
 ]
+
+# When thinking about making a webpage inside of Django
+# 1. Create a view Function
+# 2. Create your html page
+# 3. Create a path inside of urls.py (main_app)
--- main_app/views.py
+++ main_app/views.py
@@ -8,3 +8,25 @@ def about(request): # Call about in our urls.py
 
 def index(request):
     return render(request, 'index.html')
+
+class Cat:
+    def __init__(self, name, breed, description, age):
+        self.name = name
+        self.breed = breed
+        self.description = description
+        self.age = age
+cats = [
+    Cat('Lolo', 'tabby', 'foul little demon', 3),
+    Cat('Sachi', 'tortoise shell', 'diluted tortoise shell', 0),
+    Cat('Raven', 'black tripod', '3 legged cat', 4)
+]
+
+def cats_index(request):
+    data = {
+        'cats': cats # Way to grab our data from above
+    }
+    return render(request, 'cats/index.html', data)
+
+
+
+# Before we create our next function, we are going to make a class
